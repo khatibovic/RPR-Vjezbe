e7cdf3b66c7d560f7edf6659f4de0db16282041a
khatibovic <khatibovic1@etf.unsa.ba>
2023-11-05 17:38:30 +0100

    Dodana klasa FiksniBroj i pobrojani tip Grad

--- src/main/java/org/example/FiksniBroj.java
+++ src/main/java/org/example/FiksniBroj.java
@@ -1,2 +1,20 @@
-package org.example;public class FiksniBroj {
+package org.example;
+
+public class FiksniBroj extends TelefonskiBroj{
+    private Grad grad;
+    private String broj;
+    @Override
+    public String ispisi() {
+        return null;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    public FiksniBroj(Grad grad, String broj) {
+        this.grad = grad;
+        this.broj = broj;
+    }
 }
--- src/main/java/org/example/Grad.java
+++ src/main/java/org/example/Grad.java
@@ -1,2 +1,18 @@
-package org.example;public enum Grad {
+package org.example;
+
+public enum Grad {
+    BIHAC("037"),
+    ORASJE("031"),
+    TUZLA("035"),
+    ZENICA("032"),
+    GORAZDE("038"),
+    TRAVNIK("030"),
+    MOSTAR("036"),
+    SIROKI("039"),
+    SARAJEVO("033"),
+    LIVNO("034"),
+    BRCKO("049");
+
+    Grad(String s) {
+    }
 }
