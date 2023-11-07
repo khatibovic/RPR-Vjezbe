7f6bf8b941dfbd20767ddc33ebea728a8e6993aa
khatibovic <khatibovic1@etf.unsa.ba>
2023-11-07 09:27:33 +0100

    Napravljena klasa MedunarodniBroj

---
+++ src/main/java/org/example/MedunarodniBroj.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class MedunarodniBroj extends TelefonskiBroj{
+    private String drzava;
+    private String broj;
+
+    public MedunarodniBroj(String drzava, String broj) {
+        this.drzava = drzava;
+        this.broj = broj;
+    }
+
+    public String getDrzava() {
+        return drzava;
+    }
+
+    public void setDrzava(String drzava) {
+        this.drzava = drzava;
+    }
+
+    public String getBroj() {
+        return broj;
+    }
+
+    public void setBroj(String broj) {
+        this.broj = broj;
+    }
+
+    @Override
+    public String ispisi() {
+        return "+"+drzava+broj;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
