ce383e548bd9f8c017d9bb1f8ab7b4725f77ba13
khatibovic <khatibovic1@etf.unsa.ba>
2023-11-07 09:58:20 +0100

    Implementirana metoda dajIme

--- src/main/java/org/example/Imenik.java
+++ src/main/java/org/example/Imenik.java
@@ -1,9 +1,10 @@
 package org.example;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Imenik {
-    private HashMap<String, TelefonskiBroj> imenik;
+    private HashMap<String, TelefonskiBroj> imenik = new HashMap<>();
     public void dodaj(String ime, TelefonskiBroj broj) {
         imenik.put(ime, broj);
     }
@@ -13,6 +14,12 @@ public class Imenik {
             return broj.ispisi();
         }
         return "Osoba sa tim imenom ne postoji u imeniku";
+    }
+    public String dajIme(TelefonskiBroj broj) {
+        for (Map.Entry<String, TelefonskiBroj> entry : imenik.entrySet()) {
+            if (entry.getValue().equals(broj)) return entry.getKey();
+        }
+        return "U imeniku ne postoji osoba sa tim imenom!";
     }
 
 }
