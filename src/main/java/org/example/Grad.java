c709d540a9d97081e4f294752c4675a365237fc1
khatibovic <khatibovic1@etf.unsa.ba>
2023-11-07 09:14:15 +0100

    U enum Grad dodan konstruktor i geter

--- src/main/java/org/example/Grad.java
+++ src/main/java/org/example/Grad.java
@@ -11,8 +11,25 @@ public enum Grad {
     SIROKI("039"),
     SARAJEVO("033"),
     LIVNO("034"),
-    BRCKO("049");
+    BRCKO("049"),
+    MRKONJICGRAD("050"),
+    BANJALUKA("051"),
+    PRIJEDOR("052"),
+    DOBOJ("053"),
+    SAMAC("054"),
+    BIJELJINA("055 "),
+    ZVORNIK("056"),
+    PALE("057"),
+    FOCA("058"),
+    TREBINJE("059");
 
-    Grad(String s) {
+    private final String pozivniBroj;
+
+    Grad(String pozivniBroj) {
+        this.pozivniBroj = pozivniBroj;
+    }
+
+    public String getPozivniBroj() {
+        return pozivniBroj;
     }
 }
