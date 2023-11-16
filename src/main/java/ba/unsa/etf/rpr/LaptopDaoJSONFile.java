3248a0d70220d6c224baf24fa0b0a7511bb81058
khatibovic <khatibovic1@etf.unsa.ba>
2023-11-16 13:52:58 +0100

    Kreirane tri dodatne klase vezane za zadatak

---
+++ src/main/java/ba/unsa/etf/rpr/LaptopDaoJSONFile.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class LaptopDaoJSONFile {
+    private File file;
+    private ArrayList<Laptop> laptopi;
+}
---
+++ src/main/java/ba/unsa/etf/rpr/LaptopDaoSerializableFile.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class LaptopDaoSerializableFile {
+    private File file;
+    private ArrayList<Laptop> laptopi;
+
+
+}
