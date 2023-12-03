d6b8d6416311b30b503f569bd4ad86b951eea170
khatibovic <khatibovic1@etf.unsa.ba>
2023-12-03 22:23:51 +0100

    Implementirana klasa InformacijeOStudentu

---
+++ src/main/java/org/example/InformacijeOStudentu.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class InformacijeOStudentu extends LicneInformacije{
+    private String godinaStudija;
+    private String brojIndexa;
+
+    public InformacijeOStudentu(String ime, String prezime, String godinaStudija, String brojIndexa) {
+        super(ime, prezime);
+        this.godinaStudija = godinaStudija;
+        this.brojIndexa = brojIndexa;
+    }
+
+    public String getGodinaStudija() {
+        return godinaStudija;
+    }
+
+    public void setGodinaStudija(String godinaStudija) {
+        this.godinaStudija = godinaStudija;
+    }
+
+    public String getBrojIndexa() {
+        return brojIndexa;
+    }
+
+    public void setBrojIndexa(String brojIndexa) {
+        this.brojIndexa = brojIndexa;
+    }
+}
