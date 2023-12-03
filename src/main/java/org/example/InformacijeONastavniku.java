df41e671c31bb74459ee96b545837ed16a926bdc
khatibovic <khatibovic1@etf.unsa.ba>
2023-12-03 22:26:44 +0100

    Implementirana klasa InformacijeOStudentu

---
+++ src/main/java/org/example/InformacijeONastavniku.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class InformacijeONastavniku extends LicneInformacije{
+    private String titula;
+
+    public InformacijeONastavniku(String ime, String prezime, String titula) {
+        super(ime, prezime);
+        this.titula = titula;
+    }
+
+    public String getTitula() {
+        return titula;
+    }
+
+    public void setTitula(String titula) {
+        this.titula = titula;
+    }
+}
